ba0b4ca5e07b3a904cc94ae774d477a5a787dbbe
Ricardo Takuno <takunoricardo@gmail.com>
2017-08-24 22:09:28 -0300

    Aplicativo Test Drive

---
+++ AppTestDrive/AppTestDrive/AppTestDrive/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTestDrive
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+    }
+	public partial class MainPage : ContentPage
+	{
+        public List<Veiculo> Veiculos { get; set; }
+		public MainPage()
+		{
+			InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo {Nome = "Azera V6", Preco = 60000 },
+                new Veiculo {Nome = "Fista 2.0", Preco = 50000 },
+                new Veiculo {Nome = "HB20 S", Preco = 40000 },
+            };
+            this.BindingContext = this;
+		}
+        private void ListViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            DisplayAlert("Test Drive", string.Format ("Você tocou no modelo '{0}', que custa {1}", veiculo.Nome, veiculo.PrecoFormatado), "ok");
+        }
+	}
+}
